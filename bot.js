b2a01f656c5a9e3519e515e116618955b80ab2c4
WallBall22 <44549704+WallBall22@users.noreply.github.com>
2018-11-24 14:47:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dream | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
+client.user.setGame(`NeonBot | by ╲⎝ ★ 𝟛𝓶r ★ ⎠╱`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
